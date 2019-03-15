deac364e44dabbc774c6b7da5e7a91e882c80b12
Wu Muxian <wumuxian@sensetime.com>
2019-03-15 17:55:12 +0800

    supported authentication

--- rtsp-util/main.go
+++ rtsp-util/main.go
@@ -4,10 +4,9 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 
-	"rtsp"
+	"github.com/WUMUXIAN/rtsp"
 )
 
 func init() {
@@ -31,39 +30,50 @@ func main() {
 	if len(flag.Args()) >= 1 {
 		rtspUrl := flag.Args()[0]
 
-		sess := rtsp.NewSession()
-		res, err := sess.Options(rtspUrl)
+		sess, err := rtsp.NewSession(rtspUrl)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		fmt.Println("Options:")
+		res, err := sess.Options()
+		if err != nil {
+			log.Fatalln(err)
+		}
 		fmt.Println(res)
 
-		res, err = sess.Describe(rtspUrl)
-		if err != nil {
-			log.Fatalln(err)
+		// If we need to authenciate
+		if res.StatusCode == rtsp.Unauthorized {
+			res, err := sess.Options()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			fmt.Println(res)
 		}
-		fmt.Println("Describe:")
-		fmt.Println(res)
-
-		p, err := rtsp.ParseSdp(&io.LimitedReader{R: res.Body, N: res.ContentLength})
-		if err != nil {
-			log.Fatalln(err)
-		}
-		log.Printf("%+v", p)
-
-		rtpPort, rtcpPort := 8000, 8001
-		res, err = sess.Setup(rtspUrl, fmt.Sprintf("RTP/AVP;unicast;client_port=%d-%d", rtpPort, rtcpPort))
-		if err != nil {
-			log.Fatalln(err)
-		}
-		log.Println(res)
-
-		res, err = sess.Play(rtspUrl, res.Header.Get("Session"))
-		if err != nil {
-			log.Fatalln(err)
-		}
-		log.Println(res)
+		//
+		// res, err = sess.Describe(rtspUrl)
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
+		// fmt.Println("Describe:")
+		// fmt.Println(res)
+		//
+		// p, err := rtsp.ParseSdp(&io.LimitedReader{R: res.Body, N: res.ContentLength})
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
+		// log.Printf("%+v", p)
+		//
+		// rtpPort, rtcpPort := 8000, 8001
+		// res, err = sess.Setup(rtspUrl, fmt.Sprintf("RTP/AVP;unicast;client_port=%d-%d", rtpPort, rtcpPort))
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
+		// log.Println(res)
+		//
+		// res, err = sess.Play(rtspUrl, res.Header.Get("Session"))
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
+		// log.Println(res)
 	} else {
 		r, err := rtsp.ReadRequest(bytes.NewBufferString(sampleRequest))
 		if err != nil {
